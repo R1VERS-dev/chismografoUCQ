77332e6e1d1ab428aaf8004d7b7f4e31815c00b6
serkiolara <slara13475@ucq.edu.mx>
2025-07-09 22:41:25 -0600

    Carpetas de APIS por separado

--- login/login.php
+++ login/login.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Glossy</title>
+    <title>Login</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
