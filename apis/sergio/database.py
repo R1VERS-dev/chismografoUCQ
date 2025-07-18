30cffa6034c94753e6380344dc59a02ff332e395
serkiolara <slara13475@ucq.edu.mx>
2025-07-17 18:13:06 -0600

    Revert "Correción API Sergio"
    This reverts commit 37b2f8d2b0024788ba4379de95bdf5bfa6ca2652.

--- apis/sergio/database.py
+++ apis/sergio/database.py
@@ -5,10 +5,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
